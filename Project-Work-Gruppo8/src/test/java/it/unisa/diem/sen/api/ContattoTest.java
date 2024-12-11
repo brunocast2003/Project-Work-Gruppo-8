8451d422df462e6ce499f7cf46bfbb8168405e6a
bruno <bruno@172.19.222.101>
2024-12-11 18:07:52 +0100

    Implementati metodi setup, testGetNome, testGetCognome, testValidaMail e modificato testContatto

--- Project-Work-Gruppo8/src/test/java/it/unisa/diem/sen/api/ContattoTest.java
+++ Project-Work-Gruppo8/src/test/java/it/unisa/diem/sen/api/ContattoTest.java
@@ -5,6 +5,7 @@
  */
 package it.unisa.diem.sen.api;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
@@ -21,28 +22,60 @@ import static org.junit.jupiter.api.Assertions.*;
  * 
  * @date December 10, 2024
  */
+
 public class ContattoTest {
     
     private Contatto contatto1;
     private Contatto contatto2;
     private Contatto contatto3;
+    private List<String> email;
+    private List<String> numTelefono;
     
     @BeforeEach
     public void setUp() {
-        contatto1 = new Contatto("Bruno", "Grieco");
+        contatto1 = new Contatto("Giuseppe", "Russo");
         contatto2 = new Contatto(null, null);
-        contatto3 = new Contatto("Bruno", null);
-        
+        contatto3 = new Contatto("Giuseppe", null);
+        email = new ArrayList<String>(3);
+        numTelefono = new ArrayList<String>(3);
     }
     
     @Test
     public void  testContatto(){
-        assertEquals("Bruno", contatto1.getNome());
-        assertEquals("Grieco", contatto1.getCognome());
+        assertEquals("Giuseppe", contatto1.getNome());
+        assertEquals("Russo", contatto1.getCognome());
         assertEquals(null, contatto2.getNome());
         assertEquals(null, contatto2.getCognome());
-        assertEquals("Bruno", contatto3.getNome());
+        assertEquals("Giuseppe", contatto3.getNome());
+        assertEquals(null, contatto3.getCognome());
+        
+        for(String e : email){
+            assertEquals(" ", e);
+        }
+        
+        for(String n : numTelefono){
+            assertEquals(" ", n);
+        }
+        
+    }
+    
+    @Test
+    public void testGetNome(){
+        assertEquals("Giuseppe", contatto1.getNome());
+        assertEquals(null, contatto2.getNome());
+    }
+    
+    @Test
+    public void testGetCognome(){
+        assertEquals("Russo", contatto1.getCognome());
         assertEquals(null, contatto3.getCognome());
     }
-     
+    
+    @Test
+    public void testValidaEmail(){
+    assertTrue(contatto1.validaEmail("example@email.com")); // Email valida
+    assertFalse(contatto1.validaEmail("example.it")); // Email non valida
+    assertFalse(contatto1.validaEmail("example@com")); // Email non valida
+    assertFalse(contatto1.validaEmail("")); // Email vuota
+    }
 }
