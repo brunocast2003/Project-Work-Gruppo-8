b1be90f0f904306c56a75b19335a15a3660e82e9
bruno <bruno@172.19.222.101>
2024-12-13 12:24:08 +0100

    Modificata interfaccia per classe RubricaTest

--- Project-Work-Gruppo8/src/test/java/it/unisa/diem/sen/api/RubricaTest.java
+++ Project-Work-Gruppo8/src/test/java/it/unisa/diem/sen/api/RubricaTest.java
@@ -23,128 +23,53 @@ public class RubricaTest {
     public RubricaTest() {
     }
     
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
     @BeforeEach
     public void setUp() {
+        Rubrica r = new Rubrica();
     }
     
-    @AfterEach
-    public void tearDown() {
-    }
-
-    /**
-     * Test of aggiungiContatto method, of class Rubrica.
-     */
     @Test
-    public void testAggiungiContatto() {
-        System.out.println("aggiungiContatto");
-        Contatto contatto = null;
-        Rubrica instance = new Rubrica();
-        instance.aggiungiContatto(contatto);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of rimuoviContatto method, of class Rubrica.
-     */
-    @Test
-    public void testRimuoviContatto() {
-        System.out.println("rimuoviContatto");
-        Contatto contatto = null;
-        Rubrica instance = new Rubrica();
-        instance.rimuoviContatto(contatto);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of modificaContatto method, of class Rubrica.
-     */
-    @Test
-    public void testModificaContatto() {
-        System.out.println("modificaContatto");
-        Contatto contatto = null;
-        Rubrica instance = new Rubrica();
-        instance.modificaContatto(contatto);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of cercaContatto method, of class Rubrica.
-     */
-    @Test
-    public void testCercaContatto() {
-        System.out.println("cercaContatto");
-        String cerca = "";
-        Rubrica instance = new Rubrica();
-        ObservableList<Contatto> expResult = null;
-        ObservableList<Contatto> result = instance.cercaContatto(cerca);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of ordinaRubrica method, of class Rubrica.
-     */
-    @Test
-    public void testOrdinaRubrica() {
-        System.out.println("ordinaRubrica");
-        Rubrica instance = new Rubrica();
-        instance.ordinaRubrica();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getTuttiContatti method, of class Rubrica.
-     */
-    @Test
-    public void testGetTuttiContatti() {
-        System.out.println("getTuttiContatti");
-        Rubrica instance = new Rubrica();
-        List<Contatto> expResult = null;
-        List<Contatto> result = instance.getTuttiContatti();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of caricaRubrica method, of class Rubrica.
-     */
-    @Test
-    public void testCaricaRubrica() throws Exception {
-        System.out.println("caricaRubrica");
-        String nomefile = "";
-        Rubrica instance = new Rubrica();
-        Rubrica expResult = null;
-        Rubrica result = instance.caricaRubrica(nomefile);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of salvaRubrica method, of class Rubrica.
-     */
-    @Test
-    public void testSalvaRubrica() throws Exception {
-        System.out.println("salvaRubrica");
-        String nomefile = "";
-        Rubrica instance = new Rubrica();
-        instance.salvaRubrica(nomefile);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testRubrica(){
+        
     }
     
+    @Test
+    public void testAggiungiContatto(){
+        
+    }
+    
+    @Test
+    public void testRimuoviContatto(){
+        
+    }
+    
+    @Test
+    public void testModificaContatto(){
+        
+    }
+    
+    @Test
+    public void testCercaContatto(){
+        
+    }
+    
+    @Test
+    public void testOrdinaRubrica(){
+        
+    }
+    
+    @Test
+    public void testGetTuttiContatti(){
+        
+    }
+    
+    @Test
+    public void testCaricaRubrica(){
+        
+    }
+    
+    @Test
+    public void testSalvaRubrica(){
+        
+    }
 }
