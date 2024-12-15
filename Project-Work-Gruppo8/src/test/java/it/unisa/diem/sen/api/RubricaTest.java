b0fd5c16fb113af17095819e84967eb08022b025
GiovanniGrieco <ggrie@Giovannipc>
2024-12-15 21:54:48 +0100

    aggiunto commento autori

--- Project-Work-Gruppo8/src/test/java/it/unisa/diem/sen/api/RubricaTest.java
+++ Project-Work-Gruppo8/src/test/java/it/unisa/diem/sen/api/RubricaTest.java
@@ -7,43 +7,51 @@ package it.unisa.diem.sen.api;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 /**
- *
- * @author Alessandro
+ * @author Castellano Bruno
+ * @author Grieco Giovanni
+ * @author Giachetta Corradomaria
+ * @author Di Carluccio Alesssandro
+ * 
+ * @date December 10, 2024
  */
 public class RubricaTest {
-private Rubrica rubrica;
-public RubricaTest(){
-   rubrica=new  Rubrica();
+    private Rubrica rubrica;
+    
+    public RubricaTest(){
+        rubrica=new  Rubrica();
     }
-@Test 
-public void testAggiungiContatto(){
-  Contatto c=new Contatto("Bruno", "Di Carluccio");
-  rubrica.aggiungiContatto(c);
-    assertEquals(c, rubrica.getTuttiContatti().get(0));
-    assertEquals(1, rubrica.getTuttiContatti().size());
-}
-@Test
-public void testRimuoviContatto(){
-  Contatto c1=new Contatto("Pasquale", "La Mura");
-  c1.aggiungiNumeroTelefono("354205020423");
-  c1.aggiungiEmail("pasquale@example.com");
-  rubrica.aggiungiContatto(c1);
-    assertEquals(1, rubrica.getTuttiContatti().size());
-  rubrica.rimuoviContatto(c1);
-    assertEquals(0,rubrica.getTuttiContatti().size());
-    assertFalse(rubrica.getTuttiContatti().contains(c1));
-}
+    
+    @Test 
+    public void testAggiungiContatto(){
+        Contatto c=new Contatto("Bruno", "Di Carluccio");
+        rubrica.aggiungiContatto(c);
+        assertEquals(c, rubrica.getTuttiContatti().get(0));
+        assertEquals(1, rubrica.getTuttiContatti().size());
+    }
+    @Test
+    public void testRimuoviContatto(){
+        Contatto c1=new Contatto("Pasquale", "La Mura");
+        c1.aggiungiNumeroTelefono("354205020423");
+        c1.aggiungiEmail("pasquale@example.com");
+        rubrica.aggiungiContatto(c1);
+        assertEquals(1, rubrica.getTuttiContatti().size());
+        rubrica.rimuoviContatto(c1);
+        assertEquals(0,rubrica.getTuttiContatti().size());
+        assertFalse(rubrica.getTuttiContatti().contains(c1));
+    }
 
-@Test 
-public void testCercaContatto(){
- Contatto c=new Contatto("Giovanni","Giachetta");
- rubrica.aggiungiContatto(c);
-    assertEquals(1,rubrica.getTuttiContatti().size());
-    assertEquals(c, rubrica.getTuttiContatti().get(0));
-    assertTrue(rubrica.getTuttiContatti().contains(c));
-}
-@Test
+    @Test 
+    public void testCercaContatto(){
+        Contatto c=new Contatto("Giovanni","Giachetta");
+        rubrica.aggiungiContatto(c);
+        assertEquals(1,rubrica.getTuttiContatti().size());
+        assertEquals(c, rubrica.getTuttiContatti().get(0));
+        assertTrue(rubrica.getTuttiContatti().contains(c));
+    }
+    
+    @Test
     public void testOrdinaRubrica(){
         Contatto c1 = new Contatto("Alessandro", "Di Carluccio");
         c1.aggiungiNumeroTelefono("3421113445");
@@ -55,16 +63,17 @@ public void testCercaContatto(){
         assertEquals(c1, rubrica.getTuttiContatti().get(0)); 
         assertEquals(c2, rubrica.getTuttiContatti().get(1));
     }
-@Test
-public void testGetTuttiContatti(){
-    Contatto c1=new Contatto("Diego Armando","Maradona");
-     Contatto c2=new Contatto("Khvicha","Kvaratskhelia");
-  rubrica.aggiungiContatto(c1);
-  rubrica.aggiungiContatto(c2);
- List<Contatto> list2=rubrica.getTuttiContatti();
-    assertEquals(2, list2.size());
-    assertEquals(c1, list2.get(0));
-    assertEquals(c2, list2.get(1));
-}
+    
+    @Test
+    public void testGetTuttiContatti(){
+        Contatto c1=new Contatto("Diego Armando","Maradona");
+         Contatto c2=new Contatto("Khvicha","Kvaratskhelia");
+        rubrica.aggiungiContatto(c1);
+        rubrica.aggiungiContatto(c2);
+        List<Contatto> list2=rubrica.getTuttiContatti();
+        assertEquals(2, list2.size());
+        assertEquals(c1, list2.get(0));
+        assertEquals(c2, list2.get(1));
+    }
 
 }
